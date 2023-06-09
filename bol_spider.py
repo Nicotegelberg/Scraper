9714e3663c5134f2e4167d147b8cfcfd984b9af6
Nicotegelberg <130762312+Nicotegelberg@users.noreply.github.com>
2023-06-09 13:12:37 +0200

    Add files via upload

---
+++ bol_spider.py
@@ -0,0 +1,47 @@
+import scrapy
+
+class MetacriticSpider(scrapy.Spider):
+    name = "bol_spider"
+
+    #The urls for the platforms we want to scrape the product from
+    start_urls = [
+        "https://www.bol.com/nl/nl/l/games-voor-de-ps5/51867/",
+        "https://www.bol.com/nl/nl/l/games-voor-de-ps4/38904/",
+        "https://www.bol.com/nl/nl/l/games-voor-de-pc/38907/",
+        "https://www.bol.com/nl/nl/l/games-voor-de-xbox-one/38905/",
+        "https://www.bol.com/nl/nl/l/games-voor-de-xbox-series-x/51868/",
+        "https://www.bol.com/nl/nl/l/games-voor-de-nintendo-switch/38906/",
+    ]
+
+    def parse(self, response):
+        for game_link in response.css('a.product-title::attr(href)').getall():
+            yield scrapy.Request(url=f"https://www.bol.com{game_link}", callback=self.parse_game) #callback
+    
+    #Go through all the pages of the platforms
+        next_page_link = response.css('li.pagination__controls.pagination__controls--next a::attr(href)').get()
+        if next_page_link:
+            yield response.follow(next_page_link, callback=self.parse)
+
+    def parse_game(self, response):
+        title = response.css('h1.page-heading span[data-test="title"]::text').get()
+        platform = response.css('.specs__row:nth-child(2) .specs__value::text').get().strip()
+        price = response.css("span[data-test='price']::text").get().strip()
+
+        yield {
+            "Title": title,
+            "Platform": platform,
+            "Price": response.css("span[data-test='price']::text").get().strip(),
+            "Key": title.replace(" ","").lower()[:6] + "_" + title.replace(" ","").lower()[-2:] + "_" + platform.replace(" ","").lower()[:2] + "_" + platform.replace(" ","").lower()[-2:]
+        }
+
+def parse_game(self, response):
+    title = response.css('h1.page-heading span[data-test="title"]::text').get()
+    platform = response.xpath('normalize-space(.//div[@class="specs__row"][2]/div[@class="specs__value"]/text())').get()
+    price = response.css("span[data-test='price']::text").get().strip()
+    
+    return {
+        'title': title.strip() if title else None,
+        'platform': platform.strip() if platform else None,
+        'price': price,
+    }
+
